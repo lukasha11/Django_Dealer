9c1a02a8e7e4d0c30b9d842663c8f65c340a57c5
Wloodi79 <115734820+Wloodi79@users.noreply.github.com>
2023-02-05 17:44:08 +0100

    Update admin.py (producent)

--- Dealer/Produkty/admin.py
+++ Dealer/Produkty/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Produkty
+from .models import Producent, Produkty
 # Register your models here.
+
+admin.site.register(Producent)
 admin.site.register(Produkty)
