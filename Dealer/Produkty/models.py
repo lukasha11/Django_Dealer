ca816218221e9a74ecaf5c5431ed6d7880748a26
lukasha11 <84941658+lukasha11@users.noreply.github.com>
2023-02-05 18:59:30 +0100

    naprawa wyswietlania

--- Dealer/Produkty/models.py
+++ Dealer/Produkty/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Producent(models.Model):
-    def str(self):
+    def __str__(self):
         return self.nazwa
     nazwa = models.CharField(max_length=100)
 
@@ -12,7 +12,7 @@ class Producent(models.Model):
         verbose_name_plural = 'Producent'
         
 class Rodzaj(models.Model):
-    def str(self):
+    def __str__(self):
         return self.nazwa
     nazwa = models.CharField(max_length=100)
 
