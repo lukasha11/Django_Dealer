bba114c41c06659a462757ec291f903a87be2973
lukasha11 <84941658+lukasha11@users.noreply.github.com>
2023-02-05 17:45:24 +0100

    Update views

--- Dealer/Produkty/views.py
+++ Dealer/Produkty/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import  HttpResponse
+from .models import Produkty
+from .models import Producent
+
 
 # Create your views here.
+
+def index(request):
+    zapytanie = Produkty.objects.all()
+    # zapytanie = Sprzedaz.objects.all()
+    return HttpResponse(zapytanie)
