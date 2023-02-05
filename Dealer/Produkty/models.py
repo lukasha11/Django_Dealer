ab4fdbc995de08a27893f03c6a7fc5e182b93fb0
tmichalski2000 <124507987+tmichalski2000@users.noreply.github.com>
2023-02-05 17:40:52 +0100

    update models.py

--- Dealer/Produkty/models.py
+++ Dealer/Produkty/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Producent(models.Model):
+    def str(self):
+        return self.nazwa
+    nazwa = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Auto'
+        verbose_name_plural = 'Producent'
+        
 class Produkty(models.Model):
     nazwa = models.CharField(max_length=100)
     rocznik = models.DecimalField(max_digits=12, decimal_places=2)
