ed2b8e11ad0b78e1d3138614555153a57402c72a
Wloodi79 <115734820+Wloodi79@users.noreply.github.com>
2023-02-07 01:56:09 +0100

    add id

--- Dealer/Produkty/models.py
+++ Dealer/Produkty/models.py
@@ -3,25 +3,27 @@ from django.db import models
 # Create your models here.
 
 class Producent(models.Model):
+    id = models.AutoField(primary_key=True)
     nazwa = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nazwa
-    
-  
+
     class Meta:
         verbose_name_plural = 'Producent'
         
 class Rodzaj(models.Model):
+    id = models.AutoField(primary_key=True)
     nazwa = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.nazwa    
-    
+        return self.nazwa
+
     class Meta:
         verbose_name_plural = 'Rodzaj'
 
 class Produkty(models.Model):
+    id = models.AutoField(primary_key=True)
     Rodzaj = models.ForeignKey(Rodzaj, null=True, blank=True,on_delete=models.CASCADE)
     producent = models.ForeignKey(Producent, on_delete=models.CASCADE,null=True)
     nazwa = models.CharField(max_length=100)
